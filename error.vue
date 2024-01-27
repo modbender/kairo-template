b0cc94962c278b3cd6d66d18f5be71ce7f9f244f
modbender <{ID}+{username}@users.noreply.github.com>
2024-01-27 20:28:19 +0530

    error page

---
+++ error.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="error-page-container">
+    <div class="error-page-box">
+      <div class="error-page-content">
+        <h1 class="text-8xl text-white">
+          {{ error.statusCode }}
+        </h1>
+        <h2 class="text-2xl text-white">
+          {{ error.message || error.statusMessage || "" }}
+        </h2>
+        <hr class="my-5" />
+        <button class="text-white text-4xl" @click="handleError">
+          <Icon class="mb-1" name="mdi:home" />
+          Go Home
+        </button>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  error: Object,
+});
+
+const { error } = toRefs(props);
+
+const router = useRouter();
+
+const handleError = () => {
+  clearError();
+  router.push("/");
+};
+</script>
